458ee070ca21e96f2d07d1a2fe56be905c3db4fc
Rene Daniel Merlo <rmerlo@gmail.com>
2024-08-09 01:00:15 -0300

    Sincronización al 09/08/2024

--- src/Components/Destacados/ItemDest.jsx
+++ src/Components/Destacados/ItemDest.jsx
@@ -5,7 +5,7 @@ const ItemDest = ({titulo, descrip}) => {
         <>
             <div className="flex flex-col justify-center items-center p-10 text-center space-y-4">
                 <PiSealCheckDuotone color="#23DE81" size={95} />
-                <h3 className="font-Roboto text-2xl font-bold">{titulo}</h3>
+                <h3 className="font-Roboto text-[20px] md:text-2xl font-bold">{titulo}</h3>
                 <p className="font-Roboto" >{descrip}</p>
             </div>
         </>
